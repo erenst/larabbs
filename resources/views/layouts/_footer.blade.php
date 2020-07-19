49db08ebac9cccd82401fbb8409f224142b1dfca
erenst <erenst2002@hotmail.com>
2020-07-19 19:19:30 +0930

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="footer">
+
+    <div class="container">
+  
+      <p class="float-left">
+  
+        由 <a href="#" target="_blank">Erenst</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+  
+      </p>
+  
+  
+  
+      <p class="float-right"><a href="mailto:erenst2002@hotmail.com">联系Erenst</a></p>
+  
+    </div>
+  
+  </footer>
