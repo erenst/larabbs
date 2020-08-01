9f961aecbe153c97c92ca2fde60f842047d319d7
erenst <erenst2002@hotmail.com>
2020-08-01 13:45:34 +0930

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -59,3 +59,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 //User相关路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
