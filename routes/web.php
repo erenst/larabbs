32733aa3c2d2a6b65df3c40f556cd986797180cf
erenst <erenst2002@hotmail.com>
2020-08-02 17:18:25 +0930

    add slug to URL

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,8 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 //Topic相关路由
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 //Categories相关路由
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 //上次图片
