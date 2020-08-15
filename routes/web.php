650b7bfea765b1f276ef1d0d1949e383c8aff09c
erenst <erenst2002@hotmail.com>
2020-08-15 15:54:04 +0930

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -70,3 +70,5 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 //通知的路由
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+//无权限的路由
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
