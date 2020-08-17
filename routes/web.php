895a9374b4e843becdab0c67f2a3ee9caeb818d9
erenst <erenst2002@hotmail.com>
2020-08-17 20:11:58 +0930

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@
 Route::get('test', function () {
     return DB::select('select * from users');
 });
-Route::get('/', 'PagesController@root')->name('root');
+
+Route::get('/', 'TopicsController@index')->name('root');
 
 // Auth::routes();
 
